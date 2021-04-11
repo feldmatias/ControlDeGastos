d1f8698277d5f9eb8677f69ac53e5ed6c7f9603b
Matias Feld <feldmatias@gmail.com>
2021-04-11 15:19:45 -0300

    date changes

--- app/BaseModel.php
+++ app/BaseModel.php
@@ -6,7 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model{
 
-    public function getDateFormat() {
-         return 'Y-m-d H:i:s.u';
-    }
+    protected $dateFormat = 'Y-m-d H:i:sO';
 }
